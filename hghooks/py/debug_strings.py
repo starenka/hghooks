05659849a8c2ab8874c2662d4549021cc07f8c05
starenka <starenka0@gmail.com>
2013-08-05 14:19:08 +0200

    hg 2.7 changes

--- hghooks/py/debug_strings.py
+++ hghooks/py/debug_strings.py
@@ -11,23 +11,25 @@
 import re
 from hghooks import Output, grep_changed_files
 
-RE_ASSERT = 'assert False',re.compile(r'^((?!#)\s)*assert False.*')
-RE_PRINT = 'print',re.compile(r'^((?!#)\s)*print.*')
-RE_IPYTHON = 'IPython shell embedding',re.compile(r'^((?!#)\s)*IPShellEmbed\(\)\(\).*')
+RE_ASSERT = 'assert False', re.compile(r'^((?!#)\s)*assert False.*')
+RE_PRINT = 'print', re.compile(r'^((?!#)\s)*print.*')
+RE_PPRINT = 'pprint', re.compile(r'^((?!#)\s)*pprint\(.*')
+RE_IPYTHON = 'IPython shell embedding', re.compile(r'^((?!#)\s)*IPShellEmbed\(\)\(\).*')
 
 def check(ui, repo, hooktype, node, **kwargs):
-    errors = grep_changed_files(repo,node,ext='py',checks=(RE_ASSERT,RE_PRINT,RE_IPYTHON))
+    errors = grep_changed_files(repo, node, ext='py', checks=(RE_ASSERT, RE_PRINT, RE_IPYTHON, RE_PPRINT))
 
     if errors:
         out = Output()
-        out.append("You maybe didn't want to commit this, did you?..",'ERR')
-        for file,errs in errors.items():
-            out.append('file:\t%s\n%s'%(file,'-'*(len(file)+8)),'HLBOLD')
-            for line,check,string in errs:
-                out.append('%s\t%s%s (%s)'%(str(line).rjust(4),string,out.RESET,check),'HL',prepend='')
-        out.append('\n(You can disable this hook w/ --config: hg commit --config "hooks.%s.pydebugstrings=")\n'%hooktype)
+        out.append("You maybe didn't want to commit this, did you?..", 'ERR')
+        for file, errs in errors.items():
+            out.append('file:\t%s\n%s' % (file, '-' * (len(file) + 8)), 'HLBOLD')
+            for line, check, string in errs:
+                out.append('%s\t%s%s (%s)' % (str(line).rjust(4), string, out.RESET, check), 'HL', prepend='')
+        out.append(
+            '\n(You can disable this hook w/ --config: hg commit --config "hooks.%s.pydebugstrings=")\n' % hooktype)
 
         ui.warn(out.lines())
-        input = ui.promptchoice('Commit anyway? [y/n]',(('&Yes'),('&No')),default=1)
+        input = ui.promptchoice("Commit anyway? [y/n] $$ &Yes $$ &No", default=1)
         return bool(input)
     return False
